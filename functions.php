00b38af11e122c84072aa6177537072fc65f09b8
Scott van den Broek <you@example.com>
2021-12-08 16:59:40 +0100

    menu toegevoegd en nieuw plaatje voor thema

--- functions.php
+++ functions.php
@@ -3,3 +3,9 @@ function stedelijk_load_styles_scripts(){
     wp_enqueue_style('main.css', get_template_directory_uri() . '/css/stedelijk.css');
 }
 add_action('wp_enqueue_scripts','stedelijk_load_styles_scripts');
+
+function stedelijk_init_theme(){
+    register_nav_menu('main-menu', __('Hoofdmenu'));
+
+}
+add_action('init', 'stedelijk_init_theme');
