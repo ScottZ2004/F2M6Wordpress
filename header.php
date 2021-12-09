a4d187448e115365b4f83298f4dc750996460b2b
Scott van den Broek <you@example.com>
2021-12-09 16:21:57 +0100

    met scss naar css geoefend

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <?php wp_head();?>
 </head>
-<body <?php body_class();?>>
+<body <?php body_class();?>>    
 
 <header class="site-header">
     <h1><?php bloginfo('name')?></h1>
