3729b08631fe9cc0a591f68c314cd30a2801a0f4
Colin de Groot <colindegroot2004@gmail.com>
2022-01-20 11:33:58 +0100

    begin footer pushen

--- footer.php
+++ footer.php
@@ -1,4 +1,30 @@
-</div>
-<?php wp_footer();?>
+<<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <title><?php bloginfo('name'); ?></title>
+</head>
+
+<body <?php body_class(); ?>>
+    <div class=" footer">
+        <div class="footer-content"> 
+            
+            <div class="footer-left">
+                    <div class="footer-menu">
+                      <ul class="f-menu">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Blog</a></li>
+                      </ul>
+                    </div>
+            </div>
+            <div class ="foot">
+                <h2>hier komt de linker footer met linkjes</h2>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
--- page.php
+++ page.php
@@ -6,11 +6,10 @@ get_header();
         while(have_posts()): the_post();?>
 
             <h2><?php the_title() ?></h2>
-            <?php the_content()?>
 
+            <?php the_content()?>
             <hr>
 
-            <a href="<?php the_permalink();?>">Lees meer!</a>
 
         <?php endwhile;
 
