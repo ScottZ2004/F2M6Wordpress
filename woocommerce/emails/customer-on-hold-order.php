470f01b68ae8d910d43375c64f3e39e5b4732aa8
Scott van den Broek <you@example.com>
2022-01-25 15:52:22 +0100

    email template gemaakt van de wireframe

---
+++ woocommerce/emails/customer-on-hold-order.php
@@ -0,0 +1,46 @@
+<img style=";width: 67px;height: 93px;z-index: 2; display: block;margin-left: auto;margin-right: auto;" src="http://stedelijkmuseum.local/wp-content/uploads/2022/01/stedelijk-bter.png">
+
+
+<hr style="border: solid; color: black;">
+
+<div>
+    <h1 style="text-align: center">We voelen ons vereerd</h1>
+    <h2 style="text-align: center">Dat je binnenkort bij ons langskomt in het Stedelijk Museum</h2>
+    <p style="text-align: center">Bedankt dat je voor ons hebt gekozen <?php printf( esc_html__('%s','woocommerce' ), esc_html( $order->get_billing_first_name() ) ); ?>,</p>
+    <p style="text-align: center">wij gaan voor jullie zorgen dat alles tip top in orde is voor jullie</p>
+    <p style="text-align: center">bezoek aan het Stedelijk Museum.</p>
+</div>
+
+<img style="display: block;margin-left: auto;margin-right: auto;" src="http://stedelijkmuseum.local/wp-content/uploads/2022/01/stedelijk-museum.jpg" alt="Voorbeeld">
+
+<button style="display: block;margin-left: auto;margin-right: auto;">Bekijk alvast de collectie</button>
+
+<hr style="border: solid; color: black;">
+
+<h2 style="text-align: center">Jouw gegevens</h2>
+<p style="text-align: center"><b>Naam: </b><?php printf( esc_html__('%s','woocommerce' ), esc_html( $order->get_billing_first_name() ) ); ?></p>
+<p style="text-align: center"><b>Email-adres: </b><?php printf( esc_html__('%s','woocommerce' ), esc_html( $order->get_billing_email() ) ); ?></p>
+<p style="text-align: center"><b>Telefoonnummer: </b><?php printf( esc_html__('%s','woocommerce' ), esc_html( $order->get_billing_phone() ) ); ?></p>
+<br>
+<p style="text-align: center"><b>Bedrijf: </b><?php printf( esc_html__('%s','woocommerce' ), esc_html( $order->get_billing_company() ) ); ?></p>
+<p style="text-align: center"><b>Adres: </b><?php printf( esc_html__('%s','woocommerce' ), esc_html( $order->get_billing_address_1() ) ); ?></p>
+<br>
+<p style="text-align: center"><b>Aantal collega's: </b></p>
+<p style="text-align: center"><b>hapje en drankje: </b></p>
+
+<hr style="border: solid; color: black;">
+
+<div style="display: flex; justify-content: space-evenly">
+    <p><a href="https://www.facebook.com/Stedelijk/">Facebook</a></p>
+    <p><a href="https://www.instagram.com/stedelijkmuseum/?hl=en">Instagram</a></p>
+    <p><a href="https://twitter.com/stedelijk">Twitter</a></p>
+    <p><a href="https://www.youtube.com/c/StedelijkMuseumAmsterdam">Youtube</a></p>
+</div>
+<br>
+<img style=";width: 67px;height: 93px;z-index: 2; display: block;margin-left: auto;margin-right: auto;" src="http://stedelijkmuseum.local/wp-content/uploads/2022/01/stedelijk-bter.png">
+<br>
+
+<p style="text-align: center">Copyright © 2021 Stedelijk Museum Amsterdam. All rights reserved.</p>
+<br>
+<p style="text-align: center">Our mailing adress is:</p>
+<p style="text-align: center"><a href="Info@stedelijkmuseum.nl">Info@stedelijkmuseum.nl</a></p>
